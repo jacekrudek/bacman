120e16aae4ef6c4f64564f49e87b91ba33d8ddc5
Jacek <115008121+jacekrudek@users.noreply.github.com>
2024-03-14 21:04:40 +0100

    Menu class

--- pacpac/Game.cpp
+++ pacpac/Game.cpp
@@ -3,16 +3,22 @@
 void Game::initVar()
 {
 	this->window = nullptr;
+
+	this->state = State::MENU;
 }
 
+//Window properties
 void Game::initWindow()
 {
 	this->videoMode.height = 1000;
 	this->videoMode.width = 800;
 
 	this->window = new sf::RenderWindow(this->videoMode, "Pacman 1.0", sf::Style::Titlebar | sf::Style::Close);
+
+	this->window->setFramerateLimit(75);
 }
 
+//Check if window is open
 const bool Game::getWindow()
 {
 	return this->window->isOpen();
@@ -20,25 +26,124 @@ const bool Game::getWindow()
 
 void Game::update()
 {
-	this->pollEvents();
+	if (state == State::MENU)
+	{
+		this->pollMenuEvents();
+	}
+	else
+	{
+		this->pollEvents();
+	}
 }
 
 void Game::render()
 {
+	//Clear current frame
 	this->window->clear();
+
+	//Draw ingame objects depending on the games current state
+	if (state == State::MENU)
+	{
+		this->window->draw(pacman_logo);
+		switch (this->menustate)
+		{
+		case menuState::PLAY:
+			this->window->draw(t_play2);
+			this->window->draw(t_options);
+			this->window->draw(t_exit);
+			break;
+		case menuState::OPTIONS:
+			this->window->draw(t_play);
+			this->window->draw(t_options2);
+			this->window->draw(t_exit);
+			break;
+		case menuState::EXIT:
+			this->window->draw(t_play);
+			this->window->draw(t_options);
+			this->window->draw(t_exit2);
+			break;
+		}
+	}
+	else if (state == State::OPTIONS)
+	{
+
+	}
+	else
+	{
+
+	}
+	
+	//Display objects
 	this->window->display();
 }
 
 void Game::pollEvents()
 {
+	
+}
+
+void Game::pollMenuEvents()
+{
 	while (this->window->pollEvent(this->event))
 	{
-		switch (this->event.type)
+		if (this->event.type == sf::Event::Closed)
 		{
-		case sf::Event::Closed:
 			window->close();
-			break;
 		}
+		if (state == State::MENU)
+		{
+			if (this->event.type == sf::Event::KeyPressed)
+			{
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Up:
+					if (menustate == menuState::PLAY)
+					{
+						this->menustate = menuState::EXIT;
+					}
+					else if (menustate == menuState::OPTIONS)
+					{
+						this->menustate = menuState::PLAY;
+					}
+					else if (menustate == menuState::EXIT)
+					{
+						this->menustate = menuState::OPTIONS;
+					}
+					break;
+				case sf::Keyboard::Down:
+					if (menustate == menuState::PLAY)
+					{
+						this->menustate = menuState::OPTIONS;
+					}
+					else if (menustate == menuState::OPTIONS)
+					{
+						this->menustate = menuState::EXIT;
+					}
+					else if (menustate == menuState::EXIT)
+					{
+						this->menustate = menuState::PLAY;
+					}
+					break;
+				case sf::Keyboard::Enter:
+					if (menustate == menuState::PLAY)
+					{
+						this->state = State::INGAME;
+					}
+					else if (menustate == menuState::OPTIONS)
+					{
+						this->state = State::OPTIONS;
+					}
+					else if (menustate == menuState::EXIT)
+					{
+						window->close();
+					}
+					break;
+				default:
+					break;
+				}
+			}
+		}
+
 	}
 }
 
@@ -46,6 +151,7 @@ Game::Game()
 {
 	this->initVar();
 	this->initWindow();
+	
 }
 
 Game::~Game()
--- pacpac/Game.h
+++ pacpac/Game.h
@@ -9,12 +9,27 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 
-class Game
+#include "Menu.h"
+
+/**
+	enum defining states which the whole program can be in
+*/
+enum class State
 {
+	MENU,
+	OPTIONS,
+	INGAME
+};
+
+class Game : public Menu
+{
+	//the game window
 	sf::RenderWindow* window;
+	//
 	sf::Event event;
 	sf::VideoMode videoMode;
 
+	State state;
 	void initVar();
 	void initWindow();
 
@@ -27,6 +42,7 @@ public:
 	void update();
 	void render();
 	void pollEvents();
+	void pollMenuEvents();
 };
 
 
