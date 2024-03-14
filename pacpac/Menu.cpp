12795c246faeb4410a108438628d2fdd2003ccce
Jacek <115008121+jacekrudek@users.noreply.github.com>
2024-03-14 21:05:58 +0100

    Menu class fix commit

---
+++ pacpac/Menu.cpp
@@ -0,0 +1,76 @@
+#include "Menu.h"
+
+/*
+	-defining all of the options visible in main menu
+	-setting all options as not chosen 
+	-loading logo 
+*/
+Menu::Menu()
+{	
+	//default selected option set as PLAY
+	this->menustate = menuState::PLAY;
+	
+	//load and set properties of logo
+	if (!logo_texture.loadFromFile("logo.png"))
+	{
+		std::cerr << "Failed to load logo" << std::endl;
+	}
+	else
+	{
+		this->pacman_logo.setTexture(logo_texture);
+	}	
+
+	pacman_logo.setOrigin(logo_texture.getSize().x / 2.0f, logo_texture.getSize().y / 2.0f);
+
+	pacman_logo.setPosition(400, 100);
+	
+	pacman_logo.setScale(0.8, 0.8);
+
+	//load and set properties of font
+	if (!font.loadFromFile("menufont.ttf")) 
+	{		
+		std::cerr << "Failed to load font" << std::endl;
+	}
+
+	//load and set properties of menu options
+	setFonts(t_play, "Play", 300);
+	setFonts(t_play2, "Play", 300);
+	addOutline(t_play2);
+	setFonts(t_options, "Options", 400);
+	setFonts(t_options2, "Options", 400);
+	addOutline(t_options2);
+	setFonts(t_exit, "Exit", 500);
+	setFonts(t_exit2, "Exit", 500);
+	addOutline(t_exit2);
+}
+
+//defines all properties of main menu text
+void Menu::setFonts(sf::Text& text, const std::string& input, const float& y)
+{
+	text.setFont(font);
+	text.setString(input);
+	text.setFillColor(sf::Color::Yellow);
+	text.setCharacterSize(80);
+	
+	sf::FloatRect textBounds = text.getLocalBounds();
+	text.setOrigin(textBounds.width / 2.0f, textBounds.height / 2.0f);
+	text.setPosition(400, y);
+}
+
+void Menu::addOutline(sf::Text& text)
+{
+	text.setOutlineThickness(3.0f);
+	text.setOutlineColor(sf::Color::Red);
+}
+
+
+
+
+Menu::~Menu()
+{
+
+}
+
+
+
+
---
+++ pacpac/Menu.h
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+//sfml libraries
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+enum class menuState {
+	PLAY,
+	OPTIONS,
+	EXIT
+};
+
+class Menu
+{
+protected:
+	sf::Texture logo_texture;
+	sf::Sprite pacman_logo;
+	sf::Font font;
+	sf::Text t_play;
+	sf::Text t_play2;
+	sf::Text t_options;
+	sf::Text t_options2;
+	sf::Text t_exit;
+	sf::Text t_exit2;
+
+	menuState menustate;
+
+public:
+	Menu();
+	~Menu();
+
+	void setFonts(sf::Text& text, const std::string& input, const float& y);
+	void addOutline(sf::Text& text);
+	void pollMenuEvents(sf::RenderWindow* window);
+};
+
+#endif
+
